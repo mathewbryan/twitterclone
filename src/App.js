5445bdd852e7a5fe21045032a742124d8bc10663
Mathew Bryan <bryan.mathew@googlemail.com>
2019-08-14 14:28:48 +0100

    adding trends column

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
+import cat from './images/griefandloss.jpg';
+
+
+
 
 
 class Trends extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <div className="trendsContainer">
-      <h1 className="TrendsTitle"> Trends for you <img id="cog" src=""/></h1>
-      <div>
-        <TopTrend name="Mia" tweets=" 101K" hashtag="#somethingwitty" />
-        <TrendItem name="Tom Jones" tweets=" 101K" hashtag="#leaveyourhaton"/>
-        <TrendItem name="NHS England" tweets=" 99K" hashtag="#money4nurses"/>
-        <TrendItem name="Tim Cook" tweets=" 1.1M" hashtag="#flatearth"/>
-        <TopTrend name="Cat" tweets=" 35M" hashtag="#catsoftwitter" />
-        <a id="showMore"> Show More </a>
+        <h1 className="TrendsTitle"> Trends for you <img id="cog" src="" /></h1>
+        <div>
+          <TrendItem name="Tom Jones" tweets=" 101K" hashtag="#leaveyourhaton" image={cat}/>
+          <TopTrend name="Mia" tweets=" 101K" hashtag="#somethingwitty" />
+          <TrendItem name="NHS England" tweets=" 99K" hashtag="#money4nurses" />
+          <TrendItem name="Tim Cook" tweets=" 1.1M" hashtag="#flatearth" />
+          <TopTrend name="Cat" tweets=" 35M" hashtag="#catsoftwitter" image=""/>
+          <a id="showMore"> Show More </a>
         </div>
       </div>
 
@@ -22,14 +26,17 @@ class Trends extends React.Component {
 }
 
 class TopTrend extends React.Component {
-  render(){
-    return(
+  
+
+  render() {
+    
+    return (
       <div className="topTrendingTweetContainer">
-        <div className="trendingTweetContent">  
-        <h3>Trending United Kingdom</h3>        
-        {this.props.name} Tweeted <br/>
-        {this.props.hashtag} <br/>
-        {this.props.tweets} Tweets
+        <div className="trendingTweetContent">
+          <h3>Trending United Kingdom</h3>
+          <p> {this.props.name} Tweeted <br /></p>
+          <p>  {this.props.hashtag} <br /></p>
+          <p> {this.props.tweets} Tweets</p>
         </div>
       </div>
 
@@ -38,18 +45,15 @@ class TopTrend extends React.Component {
 }
 
 
-
-
 class TrendItem extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <div className="trendItemContainer">
-        <div className="">    
-        <h3>Trending United Kingdom</h3>      
-        {this.props.name} Tweeted<br/> 
-        {this.props.hashtag}
-        <br/>
-        {this.props.tweets} Tweets
+        <div className="">
+          <h3>Trending United Kingdom</h3>
+         <p> {this.props.name} Tweeted<br /></p>
+         <p>  {this.props.hashtag}</p>
+          <p>  {this.props.tweets} Tweets</p>
         </div>
       </div>
     )
