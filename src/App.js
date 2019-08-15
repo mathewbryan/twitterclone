86add86bc61cca9407a20fe58d5ea7c56891bfbf
Mathew Bryan <bryan.mathew@googlemail.com>
2019-08-15 12:07:45 +0100

    side pannel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import './App.css';
 import cat from './images/griefandloss.jpg';
+import cog from './images/cog.svg';
+import search from './images/search.svg';
+import sky from './images/skyimage.jpeg';
 
-
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Trends />
+      </div>
+    )
+  }
+}
 
 
 
 class Trends extends React.Component {
   render() {
     return (
+      <div className="searchBox">
+      <Search/>
       <div className="trendsContainer">
-        <h1 className="TrendsTitle"> Trends for you <img id="cog" src="" /></h1>
+        <h1 className="TrendsTitle"> Trends for you <img id="cog" src={cog} /></h1>
         <div>
-          <TrendItem name="Tom Jones" tweets=" 101K" hashtag="#leaveyourhaton" image={cat}/>
-          <TopTrend name="Mia" tweets=" 101K" hashtag="#somethingwitty" />
+          <TrendItem name="Tom Jones" tweets=" 101K" hashtag="#leaveyourhaton" />
+          <TopTrend name="Mia" tweets=" 101K" hashtag="#somethingwitty" trendimage={sky}  />
           <TrendItem name="NHS England" tweets=" 99K" hashtag="#money4nurses" />
           <TrendItem name="Tim Cook" tweets=" 1.1M" hashtag="#flatearth" />
-          <TopTrend name="Cat" tweets=" 35M" hashtag="#catsoftwitter" image=""/>
+          <TopTrend name="Cat" tweets=" 35M" hashtag="#catsoftwitter" trendimage={cat} />
           <a id="showMore"> Show More </a>
+          <Follow />
         </div>
       </div>
+      </div>
+    )
+  }
+}
 
+class Search extends React.Component {
+  state = {
+    textEntry: []
+  }
+  render() {
+    return(
+      <div className="searchArea"> 
+      <input id="searchfield" type="text"/>
+      <img id="searchIcon" src={search}/>
+      <h2 id="searchTitle"></h2>
+      </div>
     )
   }
 }
@@ -37,9 +66,9 @@ class TopTrend extends React.Component {
           <p> {this.props.name} Tweeted <br /></p>
           <p>  {this.props.hashtag} <br /></p>
           <p> {this.props.tweets} Tweets</p>
+          <img id="trendImage" src={this.props.trendimage}/>
         </div>
       </div>
-
     )
   }
 }
@@ -49,7 +78,7 @@ class TrendItem extends React.Component {
   render() {
     return (
       <div className="trendItemContainer">
-        <div className="">
+        <div className="trendItem">
           <h3>Trending United Kingdom</h3>
          <p> {this.props.name} Tweeted<br /></p>
          <p>  {this.props.hashtag}</p>
@@ -60,7 +89,28 @@ class TrendItem extends React.Component {
   }
 }
 
+class Follow extends React.Component{
+  render(){
+    return(
+      <div className="whotofollow">
+        <h1> Who to follow </h1>
+        <PeopletoFollow followImage="" name="Marty" />
+        <PeopletoFollow name="Biff" />
+        <PeopletoFollow name="Jenny" />
+      </div>
+    )
+  }
+}
+
+const PeopletoFollow = (props) => {
+  return(
+    <div className="PeopletoFollow">
+    {props.name}
+    <button id="followButton"> Follow </button>
+    </div>
+  )
+}
 
 
 
-export default Trends;
+export default App;
