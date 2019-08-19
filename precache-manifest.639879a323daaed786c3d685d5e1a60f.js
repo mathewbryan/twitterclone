5be8c4c0d351483057a9eba96757857c5bd4c89f
Mathew Bryan <bryan.mathew@googlemail.com>
2019-08-19 13:36:58 +0100

    Updates

---
+++ precache-manifest.639879a323daaed786c3d685d5e1a60f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312e39c10a627361be02f0d3285383bd",
+    "url": "/twitterclone/index.html"
+  },
+  {
+    "revision": "154396da38fae4e9eaae",
+    "url": "/twitterclone/static/css/main.85e4a3ef.chunk.css"
+  },
+  {
+    "revision": "455cee3179d48b3f34f6",
+    "url": "/twitterclone/static/js/2.a3e4b7d9.chunk.js"
+  },
+  {
+    "revision": "154396da38fae4e9eaae",
+    "url": "/twitterclone/static/js/main.7bb1893f.chunk.js"
+  },
+  {
+    "revision": "98559562f831d6cafaaf",
+    "url": "/twitterclone/static/js/runtime~main.03be0115.js"
+  },
+  {
+    "revision": "8280ca4ea1b05bf9cd3fa9c9073f5c3c",
+    "url": "/twitterclone/static/media/bbctwitter.8280ca4e.jpg"
+  },
+  {
+    "revision": "3e0b6dd21b73c5096c01b0710a7a37ef",
+    "url": "/twitterclone/static/media/cog.3e0b6dd2.svg"
+  },
+  {
+    "revision": "548a4a96ecf34d4dcc1004197d5d88d2",
+    "url": "/twitterclone/static/media/gerrardslip.548a4a96.jpg"
+  },
+  {
+    "revision": "c118e9f952780671945f370f341a8a76",
+    "url": "/twitterclone/static/media/griefandloss.c118e9f9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twitterclone/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "26618378ed9ead7be496a0f16cebb0fc",
+    "url": "/twitterclone/static/media/search.26618378.svg"
+  },
+  {
+    "revision": "9eb93ab81286bdddcbcd1c9cf7e131c5",
+    "url": "/twitterclone/static/media/skyimage.9eb93ab8.jpeg"
+  }
+]);
